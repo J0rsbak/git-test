fa0b5733b3aedd1cbe9f12d449232ad947edb606
J0rsbak <estaunjorge@outlook.es>
2023-12-06 21:43:34 +0100

    git commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+void login(){
+
+    string identifier;
+    string password;
+    
+    cout<<"Identifier: ";
+    getline(cin,identifier);
+    
+    cout<<"Password: ";
+    getline(cin,password);
+    
+
+}
+
+
+int main(){
+    login();
+
+
+
+
+
+
+
+}
