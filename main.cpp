9265ce9f3bc644b91882037a8ed0416e4566dc69
J0rsbak <estaunjorge@outlook.es>
2023-12-10 23:39:41 +0100

    commit main.cpp

--- main.cpp
+++ main.cpp
@@ -5,36 +5,14 @@ using namespace std;
 bool login_bool = true;
 
 bool verification(string identifier, string password){
-    int z = 0;
-    string aux[2];
     string line;
-    ifstream base("identifier_and_password_base.txt");
+    fstream base;
+    base.open("identifier_and_password_base.txt");
     if (!base.is_open()){
         cerr<<"Server Not Found 404."<<endl;
         return false;
     }else{
-            getline(base,line);
-            z = 0;
-            aux[0].clear();
-            aux[1].clear();
-            cout<<line.size();
-            for(int i = 0; i<line.size();i++){
-                cout<<"wow";
-                if(line[i]==' '){
-                    z++;
-                }else if(z==0){
-                    aux[0]+=line[i];
-                }else{
-                    aux[1]+=line[i];  
-                }
-            }
-            if(aux[0]=="o" && aux[1]=="o"){
-                cout<<"lol";
-                return true;
-            }else{
-                cout<<"no";
-                return false;
-            }
+            
         }
     
 }
