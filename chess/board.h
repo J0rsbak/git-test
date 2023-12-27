2a48872e2bf7ecc53966ea3bf24660187587e32b
J0rsbak <estaunjorge@outlook.es>
2023-12-27 21:22:02 +0100

    commit

--- chess/board.h
+++ chess/board.h
@@ -2,67 +2,67 @@
 #define BOARD_H
 #include <string>
 #include <iostream>
-
+#include "pieces.h"
 using namespace std;
-
+string type;
 string input_square;
 int input_position[2];
 int output_position[2];
 void converter(){
     switch (input_square[0]) {
         case 'a':
-            output_position[0] = 0;
+            output_position[1] = 0;
             break;
         case 'b':
-            output_position[0] = 1;
+            output_position[1] = 1;
             break;
         case 'c':
-            output_position[0] = 2;
+            output_position[1] = 2;
             break;
         case 'd':
-            output_position[0] = 3;
+            output_position[1] = 3;
             break;
         case 'e':
-            output_position[0] = 4;
+            output_position[1] = 4;
             break;
         case 'f':
-            output_position[0] = 5;
+            output_position[1] = 5;
             break;
         case 'g':
-            output_position[0] = 6;
+            output_position[1] = 6;
             break;
         case 'h':
-            output_position[0] = 7;
+            output_position[1] = 7;
             break;
     }
 
     switch (input_square[1]) {
         case '1':
-            output_position[1] = 7;
+            output_position[0] = 7;
             break;
         case '2':
-            output_position[1] = 6;
+            output_position[0] = 6;
             break;
         case '3':
-            output_position[1] = 5;
+            output_position[0] = 5;
             break;
         case '4':
-            output_position[1] = 4;
+            output_position[0] = 4;
             break;
         case '5':
-            output_position[1] = 3;
+            output_position[0] = 3;
             break;
         case '6':
-            output_position[1] = 2;
+            output_position[0] = 2;
             break;
         case '7':
-            output_position[1] = 1;
+            output_position[0] = 1;
             break;
         case '8':
-            output_position[1] = 0;
+            output_position[0] = 0;
             break;
     }
-
+    //cout<<output_position[0]<<output_position[1];
 }
 
 class board{
@@ -90,27 +90,35 @@ class board{
     }
         
 };
+void identifier(){
+    
+}
+void is_valid(){
+
+}
 
 board normal;
 void ask_for_square(){
-    bool aux1 = true;
-    bool aux2 = true;
-    while(aux1 == true){
-        while(aux2 == true){
+    
+    while(true){
+        while(true){
             cout<<"Enter the square where the piece you want to move is (e.g., a1): ";
             cin>>input_square;
             converter();
             if(normal.board_current_status[output_position[0]][output_position[1]]!="-"){
-                aux2 = false;
+                identifier();
+                break;
             }
         }
         input_position[0]=output_position[0];
         input_position[1]=output_position[1];
-        while(aux1 == true){
-            cout<<"Enter the square where the piece you want to move is (e.g., a1): ";
+        while(true){
+            cout<<"Enter the square where you want the piece to move (e.g., a1): ";
             cin>>input_square;
             converter();
-            
+            if(is_valid()){
+
+            }
         }
     }
 }
